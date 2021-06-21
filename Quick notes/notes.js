8dcb13cfe0b57027712d34ee16a70b84dff6a26a
Chinmay-coder-10 <85692773+Chinmay-coder-10@users.noreply.github.com>
2021-06-21 19:50:34 +0530

    updt

--- Quick notes/notes.js
+++ Quick notes/notes.js
@@ -1,1 +1,17 @@
 console.log("This is notes.js");
+
+
+let gifpre = document.getElementById('gifpreloader');
+let gifpretext = document.getElementById('preloadertext');
+
+function startpre() {
+    gifpre.style.display = "block";
+    gifpretext.style.display = "block";
+
+    function stoppre() {
+        gifpre.style.display = "none";
+        gifpretext.style.display = "none";
+    }
+
+    setTimeout(stoppre, 450);
+}
