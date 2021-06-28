0e1a68c75409e377dc7f027dbdbeeceab2991b39
Chinmay-coder-10 <85692773+Chinmay-coder-10@users.noreply.github.com>
2021-06-28 21:13:45 +0530

    Update

--- Quick notes/notes.js
+++ Quick notes/notes.js
@@ -53,12 +53,17 @@ function showNotes() {
 <path fill-rule="evenodd" d="M11.013 1.427a1.75 1.75 0 012.474 0l1.086 1.086a1.75 1.75 0 010 2.474l-8.61 8.61c-.21.21-.47.364-.756.445l-3.251.93a.75.75 0 01-.927-.928l.929-3.25a1.75 1.75 0 01.445-.758l8.61-8.61zm1.414 1.06a.25.25 0 00-.354 0L10.811 3.75l1.439 1.44 1.263-1.263a.25.25 0 000-.354l-1.086-1.086zM11.189 6.25L9.75 4.81l-6.286 6.287a.25.25 0 00-.064.108l-.558 1.953 1.953-.558a.249.249 0 00.108-.064l6.286-6.286z"></path>
 </svg>
 </button>  
+<svg id="star" class="star-svg" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink" width="21px" height="20px">
+<path  d="M0,0.054V20h21V0.054H0z M15.422,18.129l-5.264-2.768l-5.265,2.768l1.006-5.863L1.64,8.114l5.887-0.855
+	l2.632-5.334l2.633,5.334l5.885,0.855l-4.258,4.152L15.422,18.129z"/>
+</svg>
             <div class="card-body">
                         <h5 class="card-title">Note ${index + 1}</h5>
                         <p class="card-text"> ${element}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                     </div>
-                </div>`;
+                </div>               
+         `;
   });
   let notesElm = document.getElementById("notes");
   if (notesObj.length != 0) {
@@ -117,3 +122,26 @@ function editNote(index) {
   showNotes();
 }
 
+function star() {
+    let star = document.getElementById('star')
+    console.log(star)
+    star.addEventListener('click', function() {
+      star.style.background = "black";
+    });
+    star.addEventListener('dblclick', function() {
+      star.style.background = "grey";
+    })
+}
+star();
+/*
+Further Features:
+1. Add Title
+2. Mark a note as Important
+3. Separate notes by user
+4. Sync and host to web server
+*/
+
+{/* <svg class="star-svg" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink" width="21px" height="20px">
+<path d="M0,0.054V20h21V0.054H0z M15.422,18.129l-5.264-2.768l-5.265,2.768l1.006-5.863L1.64,8.114l5.887-0.855
+	l2.632-5.334l2.633,5.334l5.885,0.855l-4.258,4.152L15.422,18.129z"/>
+</svg> */}
