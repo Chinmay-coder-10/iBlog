376c41d7aa02907dca5cf5961d7ad54adda2a5b6
Chinmay-coder-10 <85692773+Chinmay-coder-10@users.noreply.github.com>
2021-07-04 21:05:24 +0530

    updt

--- Quick notes/notes.js
+++ Quick notes/notes.js
@@ -47,7 +47,7 @@ function showNotes() {
   let html = "";
   notesObj.forEach(function (element, index) {
     html += `
-            <div aria-label="Single click text to copy" class="noteCard uniquenoteCard my-2 mx-2 card" style="width: 18rem;">
+            <div aria-label="Single click notes text to copy" class="noteCard uniquenoteCard my-2 mx-2 card" style="width: 18rem;">
             <button id="${index}" onclick="editNote(this.id)" class="btn-octicon tooltipped tooltipped-nw editbtn" type="submit" aria-label="Edit this file" data-hotkey="e" data-disable-with="">
 <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" class="octicon octicon-pencil">
 <path fill-rule="evenodd" d="M11.013 1.427a1.75 1.75 0 012.474 0l1.086 1.086a1.75 1.75 0 010 2.474l-8.61 8.61c-.21.21-.47.364-.756.445l-3.251.93a.75.75 0 01-.927-.928l.929-3.25a1.75 1.75 0 01.445-.758l8.61-8.61zm1.414 1.06a.25.25 0 00-.354 0L10.811 3.75l1.439 1.44 1.263-1.263a.25.25 0 000-.354l-1.086-1.086zM11.189 6.25L9.75 4.81l-6.286 6.287a.25.25 0 00-.064.108l-.558 1.953 1.953-.558a.249.249 0 00.108-.064l6.286-6.286z"></path>
