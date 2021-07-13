1322f8900808dcc57cafefd53eaf0dfd84a3b59b
Chinmay-coder-10 <85692773+Chinmay-coder-10@users.noreply.github.com>
2021-07-13 17:52:12 +0530

    Update temperature.js

--- Temperature-calculator/temperature.js
+++ Temperature-calculator/temperature.js
@@ -2,12 +2,10 @@ const celsiusInput = document.getElementById('celsiusInput');
 const fahrenheitInput = document.getElementById('fahrenheitInput');
 
 function celToFah() {
-    fahrenheitInput.value = celsiusInput.value*9/5+32
+    fahrenheitInput.value = celsiusInput.value*9/5+32;
 };
 function fahToCel() {
-    fahval = (fahrenheitInput.value-32)×5/9;
-    celsiusInput.value = fahval;
-   alert("Alert function completed");
+    celsiusInput.value = fahrenheitInput.value-32*9/5;
 };
 
 
