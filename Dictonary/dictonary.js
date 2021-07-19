440c8a2b5b4316314846ab02f3a74d9f39155303
Chinmay-coder-10 <85692773+Chinmay-coder-10@users.noreply.github.com>
2021-07-19 10:12:56 +0530

    Update dictonary.js

--- Dictonary/dictonary.js
+++ Dictonary/dictonary.js
@@ -1,6 +1,7 @@
 let resetIcon = document.getElementsByClassName('resetIcon')[0];
 let searchTxt = document.getElementById('searchTxt');
 let searchWrapper = document.getElementsByClassName('searchWrapper');
+
 console.log()
  
 const resetValue =()=>{
@@ -10,4 +11,7 @@ const resetValue =()=>{
     searchTxt.value = "";
 }
 
-searchWrapper.innerHTML = ``
+searchWrapper.innerHTML = `<a id="searchAnchortag" href="https://www.google.com"> 
+     View on Google 
+    <img id="searchIcon" style="cursor: pointer;" src="../Images/15-150759_search-clipart.png" alt="Search-icon">
+ </a> `
