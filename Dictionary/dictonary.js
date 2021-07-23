d1f9e73b002dde958a11ecf08fd5520679b947ae
Chinmay-coder-10 <85692773+Chinmay-coder-10@users.noreply.github.com>
2021-07-23 21:47:54 +0530

    new dict

--- Dictionary/dictonary.js
+++ Dictionary/dictonary.js
@@ -1,11 +1,15 @@
 let resetIcon = document.getElementsByClassName("resetIcon")[0];
+// const userName = prompt("Enter your name");
 const searchTxt = document.querySelector("#searchTxt");
-let vogWrapper = document.getElementsByClassName("vogWrapper")[0];
-let searchIcon = document.getElementById("searchIcon");
+//let vogWrapper = document.getElementsByClassName("vogWrapper")[0];
+let searchbtn = document.getElementById("searchbtn");
 let mainsearch = document.getElementsByClassName("mainsearch")[0];
-let viewgooglebtn = document.getElementsByClassName("viewgooglebtn")[0];
+let viewongooglebtn = document.getElementsByClassName("viewongooglebtn")[0];
+let preloader = document.getElementsByClassName("preloader")[0];
+let info = document.getElementsByClassName("definationp")[0];
 // let preloaderStart = document.getElementsByClassName("preloader-start")[0];
-let info = document.getElementsByClassName("info")[0];
+// let info = document.getElementsByClassName("info")[0];
+// document.title = `Welcome ${userName}`;
 console.log();
 
 const resetValue = () => {
@@ -13,21 +17,45 @@ const resetValue = () => {
     return;
   }
   searchTxt.value = "";
+  info.innerHTML = "";
+  viewongooglebtn.style.display = "none";
 };
-function vog() {
-  if(searchTxt.value === ""){
-    alert("Nothing to search on Google");
+function viewongoogle() {
+  if (searchTxt.value === "") {
     return;
-}
+  }
   let url = "https://www.google.com/search?q=" + searchTxt.value;
   window.open(url);
 }
-searchIcon.addEventListener("click", () => {
-  viewgooglebtn.style.display = "block"  
+searchbtn.addEventListener("click", () => {
+  // preloader.style.display = "block";
+  viewongooglebtn.style.display = "block";
+  if (searchTxt.value != "") {
+    preloader.style.display = "block";
+    //viewgooglebtn.style.display = "block";
+
+    if (info.innerHTML !== "") {
+      preloader.style.display = "none";
+      if (searchTxt.innerHTML != "") {
+        preloader.style.display = "block";
+      }
+    }
+    if (searchTxt.value != "") {
+      preloader.style.display = "block";
+    }
+  }
+  function stoppre() {
+    preloader.style.display = "none";
+    // viewgooglebtn.style.display = "block";
+    info.style.display = "block";
+  }
+  function startpre() {
+    preloader.style.display = "block";
+  }
+  setTimeout(stoppre, 500);
   let word = searchTxt.value;
   getdata(word);
 });
-
 async function getdata(word) {
   let response = await fetch(
     `https://dictionaryapi.com/api/v3/references/learners/json/${word}?key=f276c47d-117a-4e13-8554-01e72d75d1d9`
@@ -37,10 +65,3 @@ async function getdata(word) {
   let defination = data[0].shortdef[0];
   info.innerHTML = defination;
 }
-
-// function viewg() {
-//     let url = "https://www.google.com/search?q="+searchTxt.value;
-//     vogWrapper.innerHTML = `<a href="${url}">view on google</a>`
-// }
-
-viewg();
